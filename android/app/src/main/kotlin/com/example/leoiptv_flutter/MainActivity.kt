4bc439cb7f4441ec84aeae53eb75c8bd0ce22b93
devzayoud <zayoudnoura.7@gmail.com>
2026-02-28 12:25:49 +0400

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/leoiptv_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.leoiptv_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/leoiptv/leoiptv_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.leoiptv.leoiptv_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
